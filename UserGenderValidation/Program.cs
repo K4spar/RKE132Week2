2317ceeed3bc64c9df4e493e00125daf553e834b
K4spar <kaspar.salvet@tktk.ee>
2023-02-01 17:28:22 +0200

    add week2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Please, enter your gender (m/f)");
+
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter your last name:");
+
+string userLastname = (Console.ReadLine());
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastname}!");
+
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastname}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastname}!");
+}
